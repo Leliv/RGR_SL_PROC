85f25794df012386ef00bac7d52d24b4fd9755c9
Leliv <leliv_blondy@mail.ru>
2016-05-11 21:38:11 +0300

    Bold text

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,17 +49,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 58);
+            this.label1.Location = new System.Drawing.Point(10, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.Size = new System.Drawing.Size(145, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Начальная сумма:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 243);
+            this.button1.Location = new System.Drawing.Point(10, 243);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 47);
+            this.button1.Size = new System.Drawing.Size(173, 47);
             this.button1.TabIndex = 1;
             this.button1.Text = "Рассчитать и построить график";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,57 +67,57 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(9, 78);
+            this.textBox1.Location = new System.Drawing.Point(10, 78);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
             this.textBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 119);
+            this.label2.Location = new System.Drawing.Point(7, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(167, 17);
+            this.label2.Size = new System.Drawing.Size(189, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Процентная ставка (%):";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(9, 139);
+            this.textBox2.Location = new System.Drawing.Point(10, 139);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(154, 22);
+            this.textBox2.Size = new System.Drawing.Size(173, 22);
             this.textBox2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 180);
+            this.label3.Location = new System.Drawing.Point(7, 180);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(157, 17);
+            this.label3.Size = new System.Drawing.Size(177, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Количество периодов:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 321);
+            this.label4.Location = new System.Drawing.Point(7, 321);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.Size = new System.Drawing.Size(136, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "Конечная сумма:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(9, 200);
+            this.textBox3.Location = new System.Drawing.Point(10, 200);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(154, 22);
+            this.textBox3.Size = new System.Drawing.Size(173, 22);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(9, 341);
+            this.textBox4.Location = new System.Drawing.Point(10, 341);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(154, 22);
+            this.textBox4.Size = new System.Drawing.Size(173, 22);
             this.textBox4.TabIndex = 8;
             // 
             // chart1
@@ -126,7 +126,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(179, 58);
+            this.chart1.Location = new System.Drawing.Point(201, 58);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
@@ -134,7 +134,7 @@
             series1.LegendText = "Наращенная сумма";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(744, 342);
+            this.chart1.Size = new System.Drawing.Size(837, 342);
             this.chart1.TabIndex = 10;
             this.chart1.Text = "chart1";
             title1.Name = "Title1";
@@ -144,17 +144,17 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 19);
+            this.label5.Location = new System.Drawing.Point(14, 19);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(321, 17);
+            this.label5.Size = new System.Drawing.Size(368, 17);
             this.label5.TabIndex = 11;
             this.label5.Text = "Формула сложных процентов:  S = P ( 1 + i ) ^ n";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 430);
+            this.ClientSize = new System.Drawing.Size(1054, 416);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox4);
@@ -166,6 +166,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "Form1";
             this.Text = "Программа для вычисления сложных процентов и их графической иллюстрации";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
